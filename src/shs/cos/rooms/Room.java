bd81fdd62e9d4a356a6dc77247e925c0866f0a7e
lwalsh3 <lwalsh2@ggc.edu>
2018-10-24 11:17:54 -0400

    Room class upadated. Can read room info from testRoomData.txt in res folder

--- src/shs/cos/rooms/Room.java
+++ src/shs/cos/rooms/Room.java
@@ -1,4 +1,95 @@
 package shs.cos.rooms;
 
-public class Room {
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
+public class Room
+{
+	private static String seperator = "::";
+
+	private String roomName;
+	private String description;
+	private ArrayList<String> connections = new ArrayList<>();
+	private ArrayList<String> itemIDList = new ArrayList<>();
+	private ArrayList<String> puzzleIDList = new ArrayList<>();
+	private ArrayList<String> monsterIDList = new ArrayList<>();
+
+	private static Scanner input;
+	static TreeMap<String, Room> roomList = new TreeMap<>();
+	
+	public Room()
+	{
+
+	}
+
+	public void readRoomFile(File f)
+	{
+		try
+		{
+			input = new Scanner(f);
+		} catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+
+		while (input.hasNextLine())
+		{
+			String[] nextLine = input.nextLine().split(seperator);
+
+			OUTER_LOOP: if (nextLine[0].equals("ID"))
+			{
+				Room newRoom = new Room();
+				String newRoomID = nextLine[1];
+
+				while (input.hasNextLine())
+				{
+					nextLine = input.nextLine().split(seperator);
+					if (nextLine[0].equals("roomName"))
+					{
+						newRoom.roomName = nextLine[1];
+					}
+
+					if (nextLine[0].equals("desc"))
+					{
+						newRoom.description = nextLine[1];
+					}
+
+					if (nextLine[0].equals("door"))
+					{
+						newRoom.connections.add(nextLine[1]);
+					}
+
+					if (nextLine[0].equals("puzzle"))
+					{
+						newRoom.puzzleIDList.add(nextLine[1]);
+					}
+
+					if (nextLine[0].equals("item"))
+					{
+						newRoom.itemIDList.add(nextLine[1]);
+					}
+					
+					if (nextLine[0].equals("monster"))
+					{
+						newRoom.monsterIDList.add(nextLine[1]);
+					}
+
+					if (nextLine[0].equals("ID") || nextLine[0].equals("endRoom"))
+					{
+						roomList.put(newRoomID, newRoom);
+						break OUTER_LOOP;
+					}
+				}
+			}
+		}
+	}
+	
+	public TreeMap<String, Room> getMap()
+	{
+		return roomList;
+	}
 }
