9845677b56291f7ee4c407639c2157dde01e6008
lwalsh3 <lwalsh2@ggc.edu>
2018-10-29 14:37:46 -0400

    Added some code to monster class. No functionality yet, just did some set up.

--- src/shs/cos/entities/Monster.java
+++ src/shs/cos/entities/Monster.java
@@ -1,4 +1,43 @@
 package shs.cos.entities;
 
-public class Monster extends Character{
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Monster extends Character
+{
+
+	private static String seperator = "::";
+
+	private String monsterName;
+	private String description;
+
+	private static Scanner input;
+
+	private TreeMap<String, Monster> monsterList = new TreeMap<>();
+
+	public Monster()
+	{
+
+	}
+
+	public static void readMonsterFile(File f)
+	{
+		// creates scanner to read file
+		try
+		{
+			input = new Scanner(f);
+		} catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+
+		// loop used to cycle through lines in file
+		while (input.hasNextLine())
+		{
+
+		}
+	}
+
 }
